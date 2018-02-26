82ade6e79d6da6f534bd58c46ea810ce5fe4e5e8
krystyna-Macleod <macl0312@algonquinlive.com>
2018-02-26 08:54:42 -0500

    Fix typo with icons

--- common/patternbot.js
+++ common/patternbot.js
@@ -348,9 +348,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/TheCreativeKrys/Documents/Semester 4/Web Development IV/Week 3/ecommerce-pattern-library
- * @version 1519652930706
+ * @version 1519653278526
  */
-const patternManifest_1519652930706 = {
+const patternManifest_1519653278526 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -678,5 +678,5 @@ const patternManifest_1519652930706 = {
   }
 };
 
-patternBotIncludes(patternManifest_1519652930706);
+patternBotIncludes(patternManifest_1519653278526);
 }());
