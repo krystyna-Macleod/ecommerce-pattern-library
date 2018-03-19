7661ad8720274e5457b4e5e1bec7c06a2f0fa903
krystyna-Macleod <macl0312@algonquinlive.com>
2018-03-19 08:59:28 -0400

    Fix header and banner

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/TheCreativeKrys/Documents/Semester 4/Web Development IV/Week 3/ecommerce-pattern-library
- * @version 1521463947272
+ * @version 1521464366437
  */
-const patternManifest_1521463947272 = {
+const patternManifest_1521464366437 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -902,5 +902,5 @@ const patternManifest_1521463947272 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521463947272);
+patternBotIncludes(patternManifest_1521464366437);
 }());
