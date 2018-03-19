7d8023d069192b5c1be6fdd75d008722e6e305fb
krystyna-Macleod <macl0312@algonquinlive.com>
2018-03-19 09:41:51 -0400

    Add changes to light banner

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/TheCreativeKrys/Documents/Semester 4/Web Development IV/Week 3/ecommerce-pattern-library
- * @version 1521466787035
+ * @version 1521466907891
  */
-const patternManifest_1521466787035 = {
+const patternManifest_1521466907891 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -902,5 +902,5 @@ const patternManifest_1521466787035 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521466787035);
+patternBotIncludes(patternManifest_1521466907891);
 }());
