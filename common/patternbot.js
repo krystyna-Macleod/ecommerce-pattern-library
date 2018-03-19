10593d56c57c963c437acc9041c8a803de64fe4d
krystyna-Macleod <macl0312@algonquinlive.com>
2018-03-19 08:52:36 -0400

    Change size of h3 in card

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/TheCreativeKrys/Documents/Semester 4/Web Development IV/Week 3/ecommerce-pattern-library
- * @version 1521463680288
+ * @version 1521463947272
  */
-const patternManifest_1521463680288 = {
+const patternManifest_1521463947272 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -902,5 +902,5 @@ const patternManifest_1521463680288 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521463680288);
+patternBotIncludes(patternManifest_1521463947272);
 }());
