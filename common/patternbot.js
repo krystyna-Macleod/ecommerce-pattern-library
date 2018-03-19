6dbb6573d57e39c1ce8d82e4e8dd9fe46df7041b
krystyna-Macleod <macl0312@algonquinlive.com>
2018-03-19 09:14:12 -0400

    Add details to cards

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/TheCreativeKrys/Documents/Semester 4/Web Development IV/Week 3/ecommerce-pattern-library
- * @version 1521464366437
+ * @version 1521465246807
  */
-const patternManifest_1521464366437 = {
+const patternManifest_1521465246807 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -902,5 +902,5 @@ const patternManifest_1521464366437 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521464366437);
+patternBotIncludes(patternManifest_1521465246807);
 }());
